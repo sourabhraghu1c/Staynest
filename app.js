c3b4b374b3c525d908cc5c82d9ccb12c239e208f
Sourabh Raghuwanshi <sourabhruhana@gmail.com>
2024-11-29 10:04:35 +0530

    changes in app.js for root route

--- app.js
+++ app.js
@@ -71,11 +71,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.get("/",(req,res)=>{
-    res.send("root is working");
-});
-
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
