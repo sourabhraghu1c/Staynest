88e2b53ee4c40b2f9191bc04932902755bdab5fb
Sourabh Raghuwanshi <sourabhruhana@gmail.com>
2025-05-03 11:57:13 +0530

    fix context issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";  // Import AuthProvider
+import { AuthProvider } from "./Context/AuthContext";
 import Layout from "./components/Layout";
 import Index from './pages/Index';
 import Login from './pages/login';
